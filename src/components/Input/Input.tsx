3f6c64dcaaa83d9e5692f4e24abd8376a0d0f1f6
Emily Domingos <emily.domingos@e-unicred.com.br>
2024-10-16 10:05:17 -0300

    Adiciona botão para mostrar senha

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./Input.module.css";
 
 type InputProps = {
@@ -23,21 +24,40 @@ const Input = ({
   onBlur,
   name
 }: InputProps) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+
+  const inputType = type === "password" && isPasswordVisible ? "text" : type;
+
   return (
     <div className={styles.wrapper}>
       <label className={styles.label} htmlFor={title}>
         {label}:
       </label>
+      <div className={styles.inputContainer}>
       <input
         name={name}
         className={styles.input}
         title={title}
         placeholder={placeholder}
-        type={type}
+        type={inputType}
         onChange={onChange}
         value={value}
         onBlur={onBlur}
       />
+      {type === "password" && (
+          <button
+            type="button"
+            className={styles.toggleButton}
+            onClick={togglePasswordVisibility}
+          >
+            {isPasswordVisible ? "Ocultar senha" : "Mostrar senha"}
+          </button>
+        )}
+      </div>
       {error && <p className={styles.error}>{error}</p>}
     </div>
   );
--- src/components/UserPhotoPost/UserPhotoPost.tsx
+++ src/components/UserPhotoPost/UserPhotoPost.tsx
@@ -1,7 +1,20 @@
-import style from "./UserPhotoPost.module.css";
+import React from 'react';
+import styles from './UserPhotoPost.module.css';
+import Input from '../Input/Input';
 
 const UserPhotoPost = () => {
-  return <div>Postar photo</div>;
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
+  return (
+    <section className={`${styles.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Legenda" type="text" name="legenda" />
+        <input type="file" name="img" id="img" onChange={handleImgChange} />
+      </form>
+    </section>
+  );
 };
 
 export default UserPhotoPost;
