50ce11cdccd06432c824b337717b4e0d412ce2a0
Emily Domingos <emily.domingos@e-unicred.com.br>
2024-10-22 18:04:47 -0300

    Feito upload de foto

--- src/components/UserPhotoPost/UserPhotoPost.tsx
+++ src/components/UserPhotoPost/UserPhotoPost.tsx
@@ -1,18 +1,86 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './UserPhotoPost.module.css';
 import Input from '../Input/Input';
+import Button from '../Button/Button';
+import useForm from '../../hooks/useForm';
+import api from '../../api/api';
+import Error from '../Helper/Error/Error';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
 
 const UserPhotoPost = () => {
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  const caption = useForm();
+  const [img, setImg] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [photoData, setPhotoData] = useState(null);
+  const navigate = useNavigate();
+  const { data } = useContext(UserContext);
+
+  useEffect(() => {
+    if (photoData) navigate('/conta');
+  }, [photoData, navigate]);
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', img.raw);
+    formData.append('userId', data.id);
+    formData.append('caption', caption.value);
+
+    const token: string = window.localStorage.getItem('access_token');
+
+    if (token) {
+      try {
+        setLoading(true);
+        const res = await api.post('/photos/upload', formData, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+        setPhotoData(res.data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+  }
+
+  function handleImgChange({ target }): void {
+    setImg({
+      preview: URL.createObjectURL(target.files[0]),
+      raw: target.files[0],
+    });
   }
 
   return (
     <section className={`${styles.photoPost} animeLeft`}>
       <form onSubmit={handleSubmit}>
-        <Input label="Legenda" type="text" name="legenda" />
-        <input type="file" name="img" id="img" onChange={handleImgChange} />
+        <Input label="Legenda" type="text" name="legenda" {...caption} />
+        <input
+          className={styles.file}
+          type="file"
+          name="img"
+          id="img"
+          onChange={handleImgChange}
+        />
+        {loading ? (
+          <Button disabled label="Enviando..." />
+        ) : (
+          <Button label="Enviar" />
+        )}
+        <Error error={error} />
       </form>
+      <div>
+        {img.preview && (
+          <div
+            className={styles.preview}
+            style={{ backgroundImage: `url('${img.preview}')` }}
+          ></div>
+        )}
+      </div>
     </section>
   );
 };
